45dafef82d9959fbfdd6fd161681177661de495b
Asha <agupta88@charlottesvilleschools.org>
2021-01-29 17:53:46 +0000

    adding string and loops

---
+++ stringsandloops.py
@@ -0,0 +1,15 @@
+# Strings and Loops
+# Written by Asha Gupta
+
+print("write sentence") 
+
+sentence = input(" ") # write out the sentence you want
+
+print(sentence.split(',')) # when printing sentence it splits it
+
+for letter in sentence: # printing letter by letter
+	print(letter)
+
+
+	
+
